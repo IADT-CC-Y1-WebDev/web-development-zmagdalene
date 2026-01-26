855c2638320eda184a5cc20708d676aecbc28744
Zoe Mbikakeu <n00256791@iadt.ie>
2026-01-26 10:27:06 +0000

    26th Jan 10:26

--- src/exercises/01-php-introduction/inc/navigation.php
+++ src/exercises/01-php-introduction/inc/navigation.php
@@ -5,8 +5,8 @@
         <li><a href="02-statements.php">Statements</a></li>
         <li><a href="03-arrays.php">Arrays</a></li>
         <li><a href="04-super-globals.php">Super Globals</a></li>
-        <li><a href="05-functions.php">Arrays</a></li>
-        <li><a href="06-scope.php">Arrays</a></li>
-        <li><a href="07-require.php">Arrays</a></li>
+        <li><a href="05-functions.php">Functions</a></li>
+        <li><a href="06-scope.php">Scope</a></li>
+        <li><a href="07-require.php">Libraries</a></li>
     </ul>
 </nav>
